61223124f9b7abbad6925374ddb61a52c56938b9
kami-ay1 <1598362368@qq.com>
2025-08-08 15:15:44 +0800

    右侧瀑布流完成

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -23,7 +23,7 @@
 							<view class="demo-tag-text">
 								{{item.tag[1]}}
 							</view>
-						</view>	
+						</view>
 						<view class='isDot' v-if='item.isDot'>
 							{{item.isDot}}
 						</view>
@@ -36,9 +36,26 @@
 						<view class="demo-title">
 							{{item.title}}
 						</view>
+						<view class="demo-price">
+							{{item.times}}
+						</view>
+						<view class="demo-tag">
+							<view class="demo-tag-owner">
+								{{item.tag[0]}}
+							</view>
+							<view class="demo-tag-text">
+								{{item.tag[1]}}
+							</view>
+						</view>
+						<view class='isDot' v-if='item.isDot'>
+							{{item.isDot}}
+						</view>
 					</view>
 				</template>
 			</up-waterfall>
+		</view>
+		<view v-if='showTopRight' class="topClass" @click="goTop()">
+			<up-icon name="arrow-upward" color="#fff" size="28"></up-icon>
 		</view>
 	</view>
 </template>
@@ -49,7 +66,9 @@
 		getHomeList
 	} from '../../api/api.js'
 	import {
-		onLoad
+		onLoad,
+		onReachBottom,
+		onPageScroll
 	} from '@dcloudio/uni-app'
 	import {
 		ref,
@@ -60,7 +79,14 @@
 	const keyword = ref('')
 	const bannerList = ref([])
 	const flowList = ref([])
-
+	// 滚动是否显示
+	const showTopRight=ref(0) 
+	const goTop=()=>{
+		uni.pageScrollTo({
+			scrollTop:0,
+			duration:300
+		})
+	}
 	onLoad(() => {
 		getBanner().then(res => {
 			console.log(res, 'res')
@@ -71,6 +97,29 @@
 			flowList.value = res
 		})
 	})
+	onReachBottom(() => {
+		console.log('触底')
+		// 模拟数据加载
+		setTimeout(() => {
+			addRandomData()
+		}, 1000)
+	})
+	onPageScroll((e)=>{
+		if(e.scrollTop>600){
+			showTopRight.value=1
+		}else{
+			showTopRight.value=0
+		}
+	})
+	const addRandomData = () => {
+		for (let i = 0; i < 10; i++) {
+			let index = uni.$u.random(0,flowList.value.length-1);
+			// 先转成字符串再转成对象，避免数组对象引用导致数据混乱
+			let item = JSON.parse(JSON.stringify(flowList.value[index]))
+			item.id = uni.$u.guid();
+			flowList.value.push(item);
+		}
+	}
 </script>
 
 <style lang='scss'>
@@ -82,57 +131,78 @@
 
 	.list {
 		margin: 30rpx 0;
-		.demo-warter{
+
+		.demo-warter {
 			margin: 10rpx 10rpx 10rpx 0;
 			background-color: '#fff';
 			border-radius: 16rpx;
 			padding: 16rpx;
 			position: relative;
 		}
-		.demo-title{
+
+		.demo-title {
 			font-size: 30rpx;
 			margin-top: 10rpx;
-			color:#303133;
+			color: #303133;
 		}
-		.demo-price{
+
+		.demo-price {
 			font-size: 24rpx;
-			color:#777;
-			margin-top:10rpx;
+			color: #777;
+			margin-top: 10rpx;
 		}
-		.demo-tag{
+
+		.demo-tag {
 			display: flex;
 			margin-top: 10rpx;
-			.demo-tag-owner{
-				border:1px solid rgb(252,163,129);
-				color:#ffaa00;
+
+			.demo-tag-owner {
+				border: 1px solid rgb(252, 163, 129);
+				color: #ffaa00;
 				font-size: 20rpx;
 				display: flex;
 				align-items: center;
 				padding: 4rpx 14rpx;
 				border-radius: 50rpx;
 			}
-			.demo-tag-text{
-				border:1px solid #00aaff;
-				color:#00aaff;
+
+			.demo-tag-text {
+				border: 1px solid #00aaff;
+				color: #00aaff;
 				margin-left: 20rpx;
 				border-radius: 50rpx;
-				padding:4rpx 14rpx;
-				font-size:20rpx;
+				padding: 4rpx 14rpx;
+				font-size: 20rpx;
 				display: flex;
 				align-items: center;
 			}
 		}
-		.isDot{
+
+		.isDot {
 			position: absolute;
-			top:20rpx;
+			top: 20rpx;
 			right: 20rpx;
 			font-size: 24rpx;
-			color:#fff;
+			color: #fff;
 			line-height: 32rpx;
 			background-color: #ff0000;
 			text-align: center;
 			border-radius: 10rpx;
 			padding: 4rpx 10rpx;
 		}
+	}
+	.topClass{
+		position: fixed;
+		bottom: 120rpx;
+		right: 30rpx;
+		background-color: rgba(0,0,0,0.5);
+		padding: 20rpx;
+		width: 44rpx;
+		height: 44rpx;
+		border-radius: 40rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		
 	}
 </style>
